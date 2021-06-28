958aa302cdee7d6650501a9316cd0998afd0199d
gkirtirajprdxn <kirtirajg@prdxn.com>
2021-06-28 22:11:54 +0530

    added custom theme folder structure

---
+++ wp-content/themes/mytheme/functions.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * My Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package My_Theme
+ */
---
+++ wp-content/themes/mytheme/header.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package My_Theme
+ */
+
+?>
