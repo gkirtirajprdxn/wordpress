29060ade5db18950b7e02db8db6673c50a07de04
gkirtirajprdxn <kirtirajg@prdxn.com>
2021-06-29 11:25:52 +0530

    added custom post type(news)

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -58,3 +58,56 @@ add_filter('upload_mimes', 'cc_mime_types');
 
 add_filter('use_block_editor_for_post', '__return_false');
 
+// Custom Post Type (News)
+function create_custom_post_type() {
+  $supports = array(
+      'title', // post title
+      'editor', // post content
+      'author', // post author
+      'thumbnail', // featured images
+      'excerpt', // post excerpt
+      'custom-fields', // custom fields
+      'comments', // post comments
+      'revisions', // post revisions
+      'post-formats', // post formats
+      'page-attributes' // page attributes 
+  );
+
+  $labels = array(
+      'name' => _x('News', 'plural'),
+      'singular_name' => _x('News', 'singular'),
+      'menu_name' => _x('News', 'admin menu'),
+      'name_admin_bar' => _x('News', 'admin bar'),
+      'add_new' => _x('Add New', 'add new'),
+      'add_new_item' => __('Add New news'),
+      'new_item' => __('New news'),
+      'edit_item' => __('Edit news'),
+      'view_item' => __('View news'),
+      'all_items' => __('All news'),
+      'search_items' => __('Search news'),
+      'not_found' => __('No news found.'),
+  );
+
+  $args = array(
+      'labels' => $labels,
+      'supports' => $supports,
+      'public' => true,
+      'taxonomies' => array( 'category', 'post_tag' ),
+      'show_ui' => true,
+      'show_in_menu' => true,
+      'show_in_nav_menus' => true,
+      'show_in_admin_bar' => true,
+      'can_export' => true,
+      'capability_type' => 'post',
+      'show_in_rest' => true,
+      'query_var' => true,
+      'rewrite' => array('slug' => 'news'),
+      'has_archive' => true,
+      'hierarchical' => true,
+      'menu_position' => 6,
+      'menu_icon' => 'dashicons-megaphone',
+  );
+
+  register_post_type('news', $args);
+}
+add_action('init', 'create_custom_post_type');
