400b4719bc5e59f5e6dc908bc2d5d8960da8025f
gkirtirajprdxn <kirtirajg@prdxn.com>
2021-06-30 12:58:34 +0530

    red-overlay section added using acf

--- wp-content/themes/mytheme/template-parts/content-redoverlay.php
+++ wp-content/themes/mytheme/template-parts/content-redoverlay.php
@@ -1,34 +1,23 @@
+
 <div class="red-overlay-container">
-  <div class="red-overlay-box">
-    <div class="bg-img">
-      <img src="" alt="img">
-    </div>
-    <div class="front-text">
-      <a href="">Rigorous System</a>
-    </div>
-  </div>
-  <div class="red-overlay-box">
-    <div class="bg-img">
-      <img src="" alt="img">
-    </div>
-    <div class="front-text">
-      <a href="">Pioneer's Ethics</a>
-    </div>
-  </div>
-  <div class="red-overlay-box">
-    <div class="bg-img">
-      <img src="" alt="img">
-    </div>
-    <div class="front-text">
-      <a href="">Unique Journeys</a>
-    </div>
-  </div>
-  <div class="red-overlay-box">
-    <div class="bg-img">
-      <img src="" alt="img">
-    </div>
-    <div class="front-text">
-      <a href="">Featured Stories</a>
-    </div>
-  </div>
+<?php if( have_rows('pioneer') ):
+  while( have_rows('pioneer') ): the_row();
+    if( have_rows('red') ):
+      while( have_rows('red') ): the_row();
+      $red_image = get_sub_field('red_image');
+      $red_text_link = get_sub_field('red_text_link'); ?>
+      <div class="red-overlay-box">
+        <div class="red-bg-img">
+          <img src="<?php echo $red_image; ?>" alt="img">
+        </div>
+        <div class="front-text">
+          <?php if( $red_text_link ): ?>
+          <a href="<?php echo $red_text_link['url']; ?>"><?php echo $red_text_link['title']; ?></a>
+          <?php endif; ?>
+        </div>
+      </div>
+      <?php endwhile;
+    endif;
+  endwhile;
+endif; ?>
 </div>
